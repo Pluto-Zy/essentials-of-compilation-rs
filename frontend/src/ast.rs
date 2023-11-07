14e467d3348f6edea863ecf39dd3bbab8d652625
ZPlutoY <38772646+Pluto-Zy@users.noreply.github.com>
2023-11-07 17:12:42 +0800

    R1: add new AST node kinds.

--- frontend/src/ast.rs
+++ frontend/src/ast.rs
@@ -13,6 +13,9 @@ pub enum BinaryOpKind {
 pub enum Expr {
     Integer(u64),
     Read,
+    // Note that we cannot use &str here, because the uniquify pass will modify the name of the
+    // variable.
+    Identifier(String),
     UnaryOperation {
         kind: UnaryOpKind,
         operand: Box<Expr>,
@@ -21,6 +24,11 @@ pub enum Expr {
         kind: BinaryOpKind,
         left_operand: Box<Expr>,
         right_operand: Box<Expr>,
+    },
+    Let {
+        variable_name: String,
+        init_expr: Box<Expr>,
+        body: Box<Expr>,
     },
 }
 
